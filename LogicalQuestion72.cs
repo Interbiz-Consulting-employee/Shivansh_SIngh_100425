47c913efb29624862d0890cf4797712866a48c61
ShivanshSingh100425 <shivansh.singh@interbizconsulting.com>
2026-02-23 12:13:42 +0530

    update

--- LogicalQuestion72.cs
+++ LogicalQuestion72.cs
@@ -3,7 +3,10 @@ using System;
 public class LogicalQuestion72
 {
     public static int MinEatingSpeed(int[] nums, int h)
-    {
+    {  
+        if (nums == null || nums.Length == 0 || h <= nums.Length-1)
+        return 0;  
+
         int left = 1;
         int right = 0;
 
@@ -33,17 +36,32 @@ public class LogicalQuestion72
         return left;
     }
 
-    public static void Main(string[] args)
+   public static void Main(string[] args)
+{
+    int[] nums1 = { 7, 15, 6, 3 };
+    int h1 = 8;
+    int result1 = MinEatingSpeed(nums1, h1);
+    
+    if ( result1 == 0 )
     {
-
-        int[] nums1 = { 7, 15, 6, 3 };
-        int h1 = 8;
-        int result1 = MinEatingSpeed(nums1, h1);
+        Console.WriteLine("Invalid input");
+    }
+    else
+    {
         Console.WriteLine("Example 1 Output: " + result1); // Expected: 5
+    }
 
-        int[] nums2 = { 25, 12, 8, 14, 19 };
-        int h2 = 5;
-        int result2 = MinEatingSpeed(nums2, h2);
+    int[] nums2 = { 25, 12, 8, 14, 19 };
+    int h2 = 5;
+    int result2 = MinEatingSpeed(nums2, h2);
+
+    if (result2 == 0 )
+    {
+        Console.WriteLine("Invalid Input");
+    }
+    else
+    {
         Console.WriteLine("Example 2 Output: " + result2); // Expected: 25
     }
+}
 }
